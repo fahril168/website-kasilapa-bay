b978c03668810388c18db631fc3aeb1cac446365
Fahril <fahrilmuhhammad@gmail.com>
2026-08-01 19:47:02 +0800

    fix(hydration): add suppressHydrationWarning to body element to prevent browser extension attribute warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} ${cormorant.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
